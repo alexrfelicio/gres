260b3977b381e26f4981edb5660e77c1ca0db8f2
Alex Felicio <aleexrodrigues2@gmail.com>
2020-09-16 21:48:49 -0300

    Initial commit
    First commit containing an initial core of the game

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Player : MonoBehaviour {
+
+    private int mBattery = 20;
+    private bool mDead = false;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("LOSE!");
+        mDead = true;
+    }
+
+    public bool AllowedToMove() {
+        Debug.Log("Battery remaining: " + mBattery);
+        bool allow = (mBattery > 0);
+        mBattery--;
+        return allow && !mDead;
+    }
+}
