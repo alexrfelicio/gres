ef4f5ada1b79990fca08ac2d7bdb40bf5b5874be
Alex Felicio <aleexrodrigues2@gmail.com>
2020-10-01 17:16:30 -0300

    Added arrows to Level Select Scene

--- Assets/Scripts/UI/PageSwiper.cs
+++ Assets/Scripts/UI/PageSwiper.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PageSwiper : MonoBehaviour {
+
+    [SerializeField] GameObject leftArrow;
+    [SerializeField] GameObject rightArrow;
 
     private int currentPanel = 1;
     private float easing = 0.5f;
@@ -17,14 +21,31 @@ public class PageSwiper : MonoBehaviour {
 
     void Update() {
         if (Input.GetKeyDown(KeyCode.LeftArrow) && currentPanel-1 > 0) {
-            currentPanel--;
-            rightMove = new Vector3(transform.position.x + panelWidth, transform.position.y, transform.position.z);
-            StartCoroutine(SmoothMove(transform.position, rightMove, easing));
+            MoveRight();
         } else if (Input.GetKeyDown(KeyCode.RightArrow) && currentPanel+1 < 6) {
-            currentPanel++;
-            leftMove = new Vector3(transform.position.x - panelWidth, transform.position.y, transform.position.z);
-            StartCoroutine(SmoothMove(transform.position, leftMove, easing));
+            MoveLeft();
         }
+
+        if (currentPanel == 1) {
+            leftArrow.GetComponent<Button>().interactable = false;
+        } else if (currentPanel == 5) {
+            rightArrow.GetComponent<Button>().interactable = false;
+        } else {
+            leftArrow.GetComponent<Button>().interactable = true;
+            rightArrow.GetComponent<Button>().interactable = true;
+        }
+    }
+
+    public void MoveLeft() {
+        currentPanel++;
+        leftMove = new Vector3(transform.position.x - panelWidth, transform.position.y, transform.position.z);
+        StartCoroutine(SmoothMove(transform.position, leftMove, easing));
+    }
+
+    public void MoveRight() {
+        currentPanel--;
+        rightMove = new Vector3(transform.position.x + panelWidth, transform.position.y, transform.position.z);
+        StartCoroutine(SmoothMove(transform.position, rightMove, easing));
     }
 
     IEnumerator SmoothMove(Vector3 source, Vector3 target, float seconds) {
